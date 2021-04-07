4e408eb7cd644024358f00e6a96afb39bd10d8b8
Andrew Bruce <me@andrewbruce.net>
2021-04-07 10:10:04 +0100

    Spring initializr
    Gradle
    Kotlin
    Spring Boot 2.4.4

---
+++ src/main/kotlin/com/boclips/terry/TerryApplication.kt
@@ -0,0 +1,11 @@
+package com.boclips.terry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TerryApplication
+
+fun main(args: Array<String>) {
+	runApplication<TerryApplication>(*args)
+}
---
+++ src/test/kotlin/com/boclips/terry/TerryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boclips.terry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TerryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
