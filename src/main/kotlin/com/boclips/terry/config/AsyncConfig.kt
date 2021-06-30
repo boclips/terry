7723205362b2e8266b1cfc24bb706f2a6043cc5c
Andrew Bruce <me@andrewbruce.net>
2021-06-30 09:14:56 +0100

    EnableAsync
    Hopefully this reduces Terry's repeated replies, since the "jobs" marked
    as @Async might actually run asynchronously.

---
+++ src/main/kotlin/com/boclips/terry/config/AsyncConfig.kt
@@ -0,0 +1,8 @@
+package com.boclips.terry.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@EnableAsync
+@Configuration
+class AsyncConfig
