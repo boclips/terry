24f534b13bf64e76fbdc30edf0978ecb0740d208
Maciej Idzikowski <maciej@boclips.com>
2023-05-17 17:03:51 +0200

    Remove coroutines for sentry report
    Sentry report gets generated multiple times. We want to test if
    coroutines are behind that
    
    [#184593851]
    Signed-off-by: Maciej Idzikowski <maciej@boclips.com>

--- src/main/kotlin/com/boclips/terry/infrastructure/outgoing/sentry/ComposeSentryReport.kt
+++ src/main/kotlin/com/boclips/terry/infrastructure/outgoing/sentry/ComposeSentryReport.kt
@@ -3,10 +3,6 @@ package com.boclips.terry.infrastructure.outgoing.sentry
 import com.boclips.terry.application.SentryReportParams
 import com.boclips.terry.application.SentryReportResponse
 import com.google.common.base.Stopwatch
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.runBlocking
 import mu.KLogging
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit.MILLISECONDS
@@ -21,15 +17,13 @@ class ComposeSentryReport(private val sentryClient: SentryClient) {
 
         val projects = sentryClient.getProjects(params)
 
-        val report = runBlocking(Dispatchers.Default) {
+        val report =
             projects
-                .map { async { sentryClient.getProjectIssues(it, params) } }
-                .awaitAll()
-                .flatten()
+                .flatMap { sentryClient.getProjectIssues(it, params) }
                 .sortedByDescending { it.count }
                 .take(params.issuesCount)
                 .joinToString(separator = System.lineSeparator()) { "${it.project!!.slug} - ${it.count} - ${it.metadata!!.type} - ${it.metadata.value}" }
-        }
+
 
         logger.info { "sentry report created in ${time.elapsed(MILLISECONDS)}ms" }
 
